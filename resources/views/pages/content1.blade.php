b03805aab4cff1277b5f7dd76a666fe458d375cb
TheV1nh <getback1110@gmail.com>
2023-03-02 17:47:07 +0700

    fix button + add font

--- resources/views/pages/content1.blade.php
+++ resources/views/pages/content1.blade.php
@@ -17,13 +17,8 @@
     {{--            </div>--}}
     {{--        </div>--}}
     {{--    </nav>--}}
-    <button
-            type="button"
-            class="btn btn-danger btn-floating"
-            id="btn-back-to-top"
-    >
-        <i class="fas fa-arrow-up"></i>
-    </button>
+        <img id="btn-back-to-top" class="btn_up_top" src="{{asset('images/arrowup.png')}}">
+
     <script>
         //Get the button
         let mybutton1 = document.getElementById("btn-back-to-top");
@@ -51,28 +46,53 @@
             document.documentElement.scrollTop = 0;
         }
     </script>
+{{--    <script>--}}
+{{--        // Get the button--}}
+{{--        let mybutton = document.getElementById("myBtn");--}}
+
+{{--        // When the user scrolls down 20px from the top of the document, show the button--}}
+{{--        window.onscroll = function() {scrollFunction()};--}}
+
+{{--        // function scrollFunction() {--}}
+{{--        //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {--}}
+{{--        //         mybutton.style.display = "block";--}}
+{{--        //     } else {--}}
+{{--        //         mybutton.style.display = "none";--}}
+{{--        //     }--}}
+{{--        // }--}}
+
+{{--        // When the user clicks on the button, scroll to the top of the document--}}
+{{--        function topFunction() {--}}
+{{--            document.body.scrollTop = 0;--}}
+{{--            document.documentElement.scrollTop = 0;--}}
+{{--        }--}}
+{{--    </script>--}}
     <ul class="header clearfix navbar">
         <div class="column menu">
-            <li class="animate__animated animate__backInLeft"><button class="mybtn logo1" onclick="topFunction()" id="myBtn" title="Go to top"><img class="logo1" src="{{ asset('images/logo.png') }}"></button></li>
+            <li class="animate__animated animate__backInLeft"><button class="mybtn logo1" onclick="topFunction()" id="myBtn" title="Go to top"><img class="logo1_mob" src="{{ asset('images/logo.png') }}"></button></li>
 
         </div>
         <div class="column content_mob animate__animated animate__backInRight">
-            <li class="menu2"><a href="#Home"><img class="home" src="{{asset('images/Home.png') }}"></a></li>
-            <li class="menu3"><a href="#logo1"><img class="logo2" src="{{asset('images/logo1.png') }}"></a></li>
+            <li class="menu2_mob"><a href="#Home"><img class="home_mob" src="{{asset('images/Home.png') }}"></a></li>
+            <li class="menu3_mob"><a href="#logo1"><img class="logo2_mob" src="{{asset('images/logo1.png') }}"></a></li>
         </div>
     </ul>
     <div class="anh_to_mob">
-        <img class="animate__animated animate__bounceIn col-10" src="{{ asset('images/text_content.png') }}">
+        <h1 class="chunen_trang">PLATFORM FOR</h1>
+        <h1 class="chunen_vang">WORLDWIDE</h1>
+        <h1 class="chunen_vang">BLOCKCHAIN</h1>
+        <h1 class="chunen_trang">GAME</h1>
+{{--        <img class="animate__animated animate__bounceIn col-10" src="{{ asset('images/text_content.png') }}">--}}
     </div>
     <div class="text_content">
         <p class="col-9 text-center">The free global platform to play hundreds of P2E games - connecting gamers,
             games, and guilds worldwide. Watch game reviews, chat, level up, and get guild verified. Beta platform coming Q2, 2022.</p>
     </div>
     <div class="themlink">
-        <a href="https://google.com"><img class="join anh_nho_mob col-3" src="{{asset('images/bình thường1.png' )}}"></a>
+        <a href="https://google.com"><img class="join anh_nho_mob col-5" src="{{asset('images/bình thường1.png' )}}"></a>
     </div>
     <div class="themlink">
-        <a href="https://fb.com"><img class="invest anh_nho_mob col-3" src="{{asset('images/Group 40.png')}}"></a>
+        <a href="https://fb.com"><img class="invest anh_nho_mob col-5" src="{{asset('images/Group 40.png')}}"></a>
     </div>
     <div class="buildingblock">
         <img class="text_anh_mob ani_bounceIn col-9" src="{{asset('images/textanh.png')}}">
