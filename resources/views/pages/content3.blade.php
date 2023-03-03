7434226d746a54dfa56c6506bcb7fdf8b6ffebb9
Chien.le <chien.hcckt@gmail.com>
2023-03-03 17:29:37 +0700

    fix_mobile_responsive_carouse

--- resources/views/pages/content3.blade.php
+++ resources/views/pages/content3.blade.php
@@ -1,33 +1,31 @@
-<div class="block3">
+<div class="block3" style="">
     <img class="bot_content ani_slideInLeft" src="{{asset('images/rm373batch16-55 1 (1).png')}}">
-    <div>
-        <img class="text_anh1 ani_slideInLeft" src="{{asset('images/Group 52.png')}}">
-    </div>
-</div>
 
-        <div class="mt-2 p-4 no-padding " style="">
-            <div class="multiple-items">
-                @foreach( $articles as $article)
-                    <div class="card" style="">
-                        @unless(empty($article->image))
-                            <img src="{{$article->image}}" class="card-img-top ps-5 pe-5 pt-5" alt="">
-                        @else
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg" class="card-img-top ps-5 pe-5 pt-5" alt="">
-                        @endif
-                        <div class="card-body ps-5 pe-5">
-                            <h5 class="card-title" style="">{{ $article->title}}</h5>
-                            <p class="card-text text-white mt-5 d-none d-md-block" style="">{!! Str::limit(strip_tags($article->content)) !!}</p>
-                        </div>
-                    </div>
-                @endforeach
+    <img class="text_anh1 ani_slideInLeft" src="{{asset('images/Group 52.png')}}">
+
+</div>
+<div class="mt-2 p-4 no-padding " style="">
+    <div class="multiple-items">
+        @foreach( $articles as $article)
+            <div class="card" style="">
+                @unless(empty($article->image))
+                    <img src="{{$article->image}}" class="card-img-top ps-5 pe-5 pt-5" alt="">
+                @else
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+                         class="card-img-top ps-5 pe-5 pt-5" alt="">
+                @endif
+                <div class="card-body ps-5 pe-5">
+                    <h5 class="card-title" style="">{{ $article->title}}</h5>
+                    <p class="card-text text-white mt-5 "
+                       style="">{!! Str::limit(strip_tags($article->content)) !!}</p>
+                    <button class="button-more">more</button>
+                </div>
             </div>
-        </div>
-
+        @endforeach
     </div>
 </div>
-
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('.multiple-items').slick({
             dots: false,
             infinite: true,
@@ -46,7 +44,7 @@
                         slidesToShow: 2,
                         slidesToScroll: 1,
                         infinite: true,
-                        dots: true,
+                        dots: false,
                         arrows: true,
                     }
                 },
@@ -55,7 +53,7 @@
                     settings: {
                         slidesToShow: 2,
                         slidesToScroll: 1,
-                        dots: true,
+                        dots: false,
                         arrows: true,
                     }
                 },
@@ -64,7 +62,7 @@
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1,
-                        dots: false,
+                        dots: true,
                         arrows: true,
                     }
                 }
