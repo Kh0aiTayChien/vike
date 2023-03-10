f379d32868436087c8ca46d271ece2225aed991d
TheV1nh <getback1110@gmail.com>
2023-03-10 17:31:57 +0700

    responsive block1 + add phone number

--- resources/views/pages/content2.blade.php
+++ resources/views/pages/content2.blade.php
@@ -1,4 +1,3 @@
-
 {{--<div class="block2">--}}
 {{--    <img class="mid_content" src="{{asset('images/Group 50.png')}}">--}}
 {{--</div>--}}
@@ -7,7 +6,8 @@
         <img class="text_anh_mob ani_bounceIn col-9" src="{{asset('images/textanh.png')}}">
     </div>
     <div class="text_content2 justify-content-center">
-        <p class="col-7 text-center ani_bounceIn aniUtil_dramatic">Everything you need to integrate blockchain technology in your apps, games and services.</p>
+        <p class="col-7 text-center ani_bounceIn aniUtil_dramatic">Everything you need to integrate blockchain
+            technology in your apps, games and services.</p>
     </div>
     <div class="icon justify-content-center">
         <img class="col-9 ani_fadeInRight aniUtil_dramatic" src="{{asset('images/Group 54.png')}}">
@@ -47,4 +47,45 @@
     </div>
 </div>
 
+<div class="block2">
+    <div class="text_head_block2 justify-content-center">
+        <img class="text_anh ani_bounceIn" src="{{asset('images/textanh.png')}}">
+    </div>
+    <div class="text_head1_block2 justify-content-center">
+        <img class="text_anh ani_bounceIn" src="{{asset('images/text.png')}}">
+    </div>
 
+    <div>
+        <div class="chinh_anh justify-content-center">
+            <img class="rectangle" src="{{asset('images/Rectangle 6.png')}}">
+        </div>
+        <div class="icon_mini_block2 justify-content-center">
+            <img class="icon1 ani_fadeInLeft aniUtil_dramatic" src="{{asset('images/Group 48.png')}}">
+        </div>
+        <div class="icon_mini1_block2 justify-content-center">
+            <img class="icon2 ani_fadeInRight aniUtil_dramatic" src="{{asset('images/Group 491.png')}}">
+        </div>
+    </div>
+    <div class="name_banner_icon3_block2 justify-content-center">
+        <img class="name_banner_icon3 ani_zoomIn aniUtil_dramatic"
+             src="{{asset('images/BACKED BY THE TOP VENTURES.png')}}">
+    </div>
+    <div class="banner_icon3_block2 justify-content-center">
+        <img class="banner_icon3 ani_bounceInDown aniUtil_dramatic" src="{{asset('images/Group 45.png')}}">
+    </div>
+    <div class="banner_icon3_block2 justify-content-center">
+        <img class="banner_icon3 ani_bounceInDown aniUtil_dramatic" src="{{asset('images/Group 46.png')}}">
+    </div>
+    <div class="banner_icon3_block2 justify-content-center">
+        <img class="banner_icon3 ani_bounceInDown aniUtil_dramatic" src="{{asset('images/Group 47.png')}}">
+    </div>
+    <div class="banner_name_block2 justify-content-center">
+        <img class="banner_name ani_bounceInDown aniUtil_dramatic" src="{{asset('images/Group 42.png')}}">
+    </div>
+    <div class="banner_icon6_block2 justify-content-center">
+        <img class="banner_icon6 ani_fadeInLeft aniUtil_dramatic" src="{{asset('images/Group 43.png')}}">
+    </div>
+    <div class="banner_icon6_block2 justify-content-center">
+        <img class="banner_icon6 ani_fadeInRight aniUtil_dramatic" src="{{asset('images/Group 441.png')}}">
+    </div>
+</div>
